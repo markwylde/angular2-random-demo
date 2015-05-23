400500a8d0e8d8bc7c99217ebea429331ea299f5
Mark Wylde <mark.wylde@verl.com>
2015-05-23 22:59:14 +0100

    Initial Commit

---
+++ app.js
@@ -0,0 +1,66 @@
+/// <reference path="typings/angular2/angular2.d.ts" />
+/// <reference path="typings/moment/moment.d.ts" />
+/// <reference path="typings/chance/chance.d.ts" />
+if (typeof __decorate !== "function") __decorate = function (decorators, target, key, desc) {
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") return Reflect.decorate(decorators, target, key, desc);
+    switch (arguments.length) {
+        case 2: return decorators.reduceRight(function(o, d) { return (d && d(o)) || o; }, target);
+        case 3: return decorators.reduceRight(function(o, d) { return (d && d(target, key)), void 0; }, void 0);
+        case 4: return decorators.reduceRight(function(o, d) { return (d && d(target, key, o)) || o; }, desc);
+    }
+};
+if (typeof __metadata !== "function") __metadata = function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var angular2_1 = require('angular2/angular2');
+// Annotation section
+var MyAppComponent = (function () {
+    function MyAppComponent() {
+        this.randomName = chance.name();
+        this.randomUrl = chance.url();
+        this.items = [];
+        this.classmap = {
+            'fa-venus': true
+        };
+    }
+    MyAppComponent.prototype.insertNew = function (amount, number) {
+        if (!amount) {
+            amount = 1;
+        }
+        var gender = chance.gender();
+        this.items.push(new Person({
+            firstName: chance.first({ gender: gender }),
+            surname: chance.last({ gender: gender }),
+            gender: gender,
+            dateOfBirth: moment(chance.birthday({ type: "adult" })).format("DD/MM/YYYY")
+        }));
+        if (amount > 1) {
+            this.insertNew(amount - 1);
+        }
+    };
+    MyAppComponent.prototype.removeItem = function (item) {
+        this.items.splice(item, 1);
+    };
+    MyAppComponent = __decorate([
+        angular2_1.Component({
+            selector: 'my-app'
+        }),
+        angular2_1.View({
+            templateUrl: 'views/home/home.html',
+            directives: [angular2_1.For, angular2_1.If]
+        }), 
+        __metadata('design:paramtypes', [])
+    ], MyAppComponent);
+    return MyAppComponent;
+})();
+exports.MyAppComponent = MyAppComponent;
+var Person = (function () {
+    function Person(data) {
+        this.firstName = data.firstName;
+        this.surname = data.surname;
+        this.gender = data.gender;
+        this.dateOfBirth = data.dateOfBirth;
+    }
+    return Person;
+})();
+angular2_1.bootstrap(MyAppComponent);
---
+++ app.ts
@@ -0,0 +1,67 @@
+/// <reference path="typings/angular2/angular2.d.ts" />
+/// <reference path="typings/moment/moment.d.ts" />
+/// <reference path="typings/chance/chance.d.ts" />
+
+import {Component, View, For, If, bootstrap} from 'angular2/angular2';
+
+// Annotation section
+@Component({
+    selector: 'my-app'
+})
+@View({
+    templateUrl: 'views/home/home.html',
+    directives: [For, If]
+})
+// Component controller
+export class MyAppComponent {
+    randomName: string;
+    randomUrl: string;
+    classmap: any;
+    items: Array<Person>;
+
+    constructor() {
+        this.randomName = chance.name();
+        this.randomUrl = chance.url();
+        this.items = [];
+        this.classmap = {
+            'fa-venus': true
+        }
+    }
+
+    insertNew(amount:?number) {
+        if (!amount) {
+            amount = 1;
+        }
+
+        var gender = chance.gender();
+        this.items.push(new Person({
+            firstName: chance.first({ gender: gender }),
+            surname: chance.last({ gender: gender }),
+            gender: gender,
+            dateOfBirth: moment(chance.birthday({type: "adult"})).format("DD/MM/YYYY")
+        }));
+
+        if (amount > 1) {
+            this.insertNew(amount - 1);
+        }
+    }
+
+    removeItem(item) {
+        this.items.splice(item, 1);
+    }
+}
+
+class Person {
+    firstName: string;
+    surname: string;
+    dateOfBirth: Date;
+
+    constructor(data) {
+        this.firstName = data.firstName;
+        this.surname = data.surname;
+        this.gender = data.gender;
+        this.dateOfBirth = data.dateOfBirth;
+    }
+}
+
+bootstrap(MyAppComponent);
